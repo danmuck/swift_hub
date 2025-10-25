7a11741351de94dfa7f1a1a90c2ab9f691ebb742
danmuck <danmuck426@gmail.com>
2025-10-24 23:52:38 -0400

    Initial Commit

---
+++ Looker/LookerApp.swift
@@ -0,0 +1,32 @@
+//
+//  LookerApp.swift
+//  Looker
+//
+//  Created by Daniel Muck on 10/24/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LookerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
